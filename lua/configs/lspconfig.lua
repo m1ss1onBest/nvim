fb533b02ef8348e26d0bd9d2495d91e2d11f6762
m1ss1onBest <m1poisthbexxxx@gmail.com>
2025-11-04 23:14:56 +0200

    replace-deprecated-parts

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,7 +1,7 @@
 -- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
-local lspconfig = require "lspconfig"
+local lspconfig = vim.lsp.config
 
 -- EXAMPLE
 local servers = { "html", "cssls", "rust_analyzer", "pyright", "clangd", "tinymist" }
